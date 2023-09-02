94536e417c4a278595b243f4e5b25d1d89ddc213
Zambrinn <zambrinfilho@gmail.com>
2023-09-01 23:19:42 -0200

    second-chance

--- scripts.js
+++ scripts.js
@@ -2,9 +2,11 @@ let chave = "68dc0f374534219e80e8270a2c34dd3b"
 function showOnScreen(dados) {
     console.log(dados)
     document.querySelector(".city").innerHTML = `Cidade: ${dados.name}`
-    document.querySelector(".temperature").innerHTML = `A temperatura local é: ${Math.floor(dados.main.temp) + "°C"}`
+    document.querySelector(".temperature").innerHTML = `A temperatura atual é: ${Math.floor(dados.main.temp) + "°C"}`
     document.querySelector(".state").innerHTML = `O clima está: ${dados.weather[0].description}`
     document.querySelector(".humidity").innerHTML = "Humidade: " + dados.main.humidity + "%"
+    document.querySelector(".min").innerHTML = `Temperatura mínima: ${Math.floor(dados.main.temp_min) + "°C"}`
+    document.querySelector(".max").innerHTML = `Temperatura máxima: ${Math.ceil(dados.main.temp_max) + "°C"}`
 }
 async function findCity(cityName) {
     let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + chave +
